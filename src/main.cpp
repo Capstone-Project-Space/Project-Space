367c2cb11543decd72144a583b2f5360241f829f
Quatroctus <kanadetachibona@gmail.com>
2021-11-30 01:36:55 -0800

    Created CMakeLists added glad glfw3 glm stb_image libraries.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+#include <gl/GL.h>
+#include <stb_image.h>
+#include <glfw3/glfw3.h>
+
+int main(int argc, char** args) {
+    printf("Hello World!\n");
+    return 0;
+}
