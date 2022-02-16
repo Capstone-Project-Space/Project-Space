4e8e3364c6245391786541a30a15eb2ca0eadcfb
Quatroctus <kanadetachibona@gmail.com>
2022-02-16 10:47:50 -0800

    Tested loading of a model file in main.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,21 @@
 #include <glm/glm.hpp>
 #include <stb_image.h>
 
+#include <src/engine/graphics/model.h>
+
 #define TESTING
 
 #if defined(TESTING)
 
-#include "./testing/tests.h"
+#include <src/testing/tests.h>
 
 #define SIZE_DUMP(type) fprintf(stderr, "sizeof(%s): %llu\n", #type, sizeof(type))
 
 int main(int argc, char** args) {
 	Test();
+
+	std::shared_ptr<Model> model = Model::CreateModel("./resources/models/capsule.obj");
+
 	return 0;
 }
 #else
