b67016582f6f69c395782ef6865ccc623d04966f
Quatroctus <kanadetachibona@gmail.com>
2022-05-02 00:47:36 -0700

    Added ButtonComponent.

---
+++ src/engine/graphics/ui/button_component.cpp
@@ -0,0 +1,75 @@
+#include "button_component.h"
+
+#include <src/engine/graphics/renderer.h>
+
+ButtonComponent::ButtonComponent(
+	const std::string_view& id, Layout* layout, std::function<void(ButtonComponent&)> clickCallback,
+	const std::string& text, std::shared_ptr<Font> font, Gravity gravity,
+	const Color& textColor, const Color& textHighlightColor, const glm::vec2& minimumDimensions
+) : UIComponent(id, layout), text(text), textFont(font), textGravity(gravity), texture(nullptr),
+	textureHighlightTint({1.0f}), minimumDimensions(minimumDimensions), clickCallback(clickCallback) {
+	if (textColor.index() == 0) this->textColor = glm::vec4{ std::get<0>(textColor), 1.0f };
+	else this->textColor = std::get<1>(textColor);
+	if (textColor.index() == 0) this->textHighlightColor = { std::get<0>(textHighlightColor), 1.0f };
+	else this->textHighlightColor = std::get<1>(textHighlightColor);
+}
+
+ButtonComponent::ButtonComponent(
+	const std::string_view& id, Layout* layout, std::function<void(ButtonComponent&)> clickCallback,
+	std::shared_ptr<Texture> texture, const Color& tintColor, const glm::vec2& minimumDimensions
+) : UIComponent(id, layout), text(""), textFont(nullptr), textGravity(Gravity::CENTER), textColor({ 1.0f }),
+	textHighlightColor({ 0.6f }), texture(texture), minimumDimensions(minimumDimensions), clickCallback(clickCallback) {
+	if (tintColor.index() == 0) this->textureHighlightTint = { std::get<0>(tintColor), 1.0f };
+	else this->textureHighlightTint = std::get<1>(tintColor);
+}
+
+ButtonComponent::ButtonComponent(
+	const std::string_view& id, Layout* layout, std::function<void(ButtonComponent&)> clickCallback,
+	const std::string& text, std::shared_ptr<Font> font, std::shared_ptr<Texture> texture,
+	const Color& textColor, const Color& textHighlightColor, const Color& tintColor, Gravity gravity,
+	const glm::vec2& minimumDimensions
+) : UIComponent(id, layout), text(text), textFont(font), textGravity(gravity), texture(texture),
+	minimumDimensions(minimumDimensions), clickCallback(clickCallback) {
+	if (textColor.index() == 0) this->textColor = glm::vec4{ std::get<0>(textColor), 1.0f };
+	else this->textColor = std::get<1>(textColor);
+	if (textColor.index() == 0) this->textHighlightColor = { std::get<0>(textHighlightColor), 1.0f };
+	else this->textHighlightColor = std::get<1>(textHighlightColor);
+	if (tintColor.index() == 0) this->textureHighlightTint = { std::get<0>(tintColor), 1.0f };
+	else this->textureHighlightTint = std::get<1>(tintColor);
+}
+
+float ButtonComponent::getContentWidth() const {
+	return std::max((float) textFont->getTextWidth(text), minimumDimensions.x);
+}
+
+float ButtonComponent::getContentHeight() const {
+	return std::max((float) textFont->getTextHeight(text), minimumDimensions.y);
+}
+
+void ButtonComponent::draw(float delta) {
+	glm::vec3 pos = this->position.value();
+	const glm::vec2 size = this->size.value();
+	bool within = this->isWithin({ Mouse::x, Mouse::y });
+	if (texture) {
+		if (within) Renderer::SubmitQuad(pos, size, Texture::GetWhiteTexture(), textureHighlightTint);
+		else Renderer::SubmitQuad(pos, size, { 0.f, 236.f / 256.f, 200.f/256.f, 1.f }, texture);
+	}
+	if (textFont) {
+		if (within) {
+			Renderer::SubmitText(text, pos, textHighlightColor, textFont, textGravity);
+		} else 
+			Renderer::SubmitText(text, pos, textColor, textFont, textGravity);
+	}
+
+}
+
+bool ButtonComponent::isWithin(const glm::vec2& pos) const {
+	const glm::vec2 size = this->size.value();
+	const glm::vec3 position = this->position.value() - (glm::vec3{ size / 2.f, 0.0f });
+	return pos.x >= position.x && pos.y >= position.y && pos.x < position.x + size.x && pos.y < position.y + size.y;
+}
+
+bool ButtonComponent::onMouseButtonReleased(const MouseButton& button) {
+	if (this->isWithin({Mouse::x, Mouse::y})) clickCallback(*this);
+	return false;
+}
---
+++ src/engine/graphics/ui/button_component.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <functional>
+#include <optional>
+#include <string_view>
+#include <variant>
+
+#include <src/engine/graphics/font.h>
+#include <src/engine/graphics/texture.h>
+#include <src/engine/graphics/ui/layout.h>
+
+using Color = std::variant<glm::vec3, glm::vec4>;
+
+class ButtonComponent : public UIComponent {
+public:
+	ButtonComponent(
+		const std::string_view& id, Layout* layout, std::function<void(ButtonComponent&)> clickCallback,
+		const std::string& text, std::shared_ptr<Font> font, Gravity gravity = Gravity::CENTER,
+		const Color& textColor = Color{ glm::vec4{1.0f} }, const Color& textHighlightColor = Color{ glm::vec4{ 0.6f } },
+		const glm::vec2& minimumDimensions = { 0.0f, 0.0f }
+	);
+
+	ButtonComponent(
+		const std::string_view& id, Layout* layout, std::function<void(ButtonComponent&)> clickCallback,
+		std::shared_ptr<Texture> texture, const Color& tint = Color{ glm::vec4{ 1.0f } },
+		const glm::vec2& minimumDimensions = { 0.0f, 0.0f }
+	);
+
+	ButtonComponent(
+		const std::string_view& id, Layout* layout, std::function<void(ButtonComponent&)> clickCallback,
+		const std::string& text, std::shared_ptr<Font>, std::shared_ptr<Texture> texture,
+		const Color& textColor = Color{ glm::vec4{ 1.0f } }, const Color& textHighlightColor = Color{ glm::vec4{.6f} },
+		const Color& tintColor = Color{ glm::vec4{ 1.0f } },
+		Gravity gravity = Gravity::CENTER, const glm::vec2& minimumDimensions = { 0.0f, 0.0f }
+	);
+
+	virtual float getContentWidth() const override;
+	virtual float getContentHeight() const override;
+
+	virtual void draw(float delta) override;
+
+	virtual bool isWithin(const glm::vec2& position) const override;
+
+	virtual bool onMouseButtonReleased(const MouseButton& button) override;
+
+protected:
+	std::function<void(ButtonComponent&)> clickCallback;
+	glm::vec2 minimumDimensions;
+
+	std::string text;
+	glm::vec4 textColor, textHighlightColor;
+	std::shared_ptr<Font> textFont;
+	Gravity textGravity;
+
+	std::shared_ptr<Texture> texture;
+	glm::vec4 textureHighlightTint;
+};
